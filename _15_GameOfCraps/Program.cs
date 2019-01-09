92d8f2895cecaf3e8e9071d4edcac5e20c996d97
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-09 17:10:21 +0100

    adds variables for special names, enum holding status of game, method for evaluating the rolls

--- _15_GameOfCraps/Program.cs
+++ _15_GameOfCraps/Program.cs
@@ -2,11 +2,70 @@
 
 namespace _15_GameOfCraps
 {
-    class MainClass
+    public class CrapsGame
     {
-        public static void Main(string[] args)
+
+        private enum DiceSum
         {
-            Console.WriteLine("Hello World!");
+            SnakeEyes = 2,
+            Trey = 3,
+            Seven = 7,
+            YoLeven = 11,
+            BoxCars = 12
+        };
+
+        private enum GameStatus
+        {
+            Win,
+            Lose,
+            Continue
+        };
+
+        private RollDice _roll = new RollDice();
+        private GameStatus _gameStatus;
+        private DiceSum _diceSum;
+        private int _numRolls;
+        private int _sum;
+        private int _point;
+
+
+        private void EvaluateRoll()
+        {
+            switch (_sum)
+            {
+                case 7:
+                    _diceSum = DiceSum.Seven;
+                    _gameStatus = GameStatus.Win;
+                    _point = 0;
+                    break;
+                case 11:
+                    _diceSum = DiceSum.YoLeven;
+                    _gameStatus = GameStatus.Win;
+                    _point = 0;
+                    break;
+                case 2:
+                    _diceSum = DiceSum.SnakeEyes;
+                    _gameStatus = GameStatus.Lose;
+                    _point = 0;
+                    break;
+                case 3:
+                    _diceSum = DiceSum.Trey;
+                    _gameStatus = GameStatus.Lose;
+                    _point = 0;
+                    break;
+                case 12:
+                    _diceSum = DiceSum.BoxCars;
+                    _gameStatus = GameStatus.Lose;
+                    _point = 0;
+                    break;
+                default:
+                    _gameStatus = GameStatus.Continue;
+                    _point = _sum;
+                    break;
+
+            }
         }
+
+
     }
 }
