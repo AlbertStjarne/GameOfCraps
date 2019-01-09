bb84cb098a520a7544206223402d48a49b82be51
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-09 16:50:34 +0100

    adds RollDice class, properties for each dice and rolling of dice method

---
+++ _15_GameOfCraps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15_GameOfCraps
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ _15_GameOfCraps/Properties/RollDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _15_GameOfCraps.Properties
+{
+    public class RollDice
+    {
+        private Random _rand = new Random();
+        public int Die1 { get; set; }
+        public int Die2 { get; set; }
+
+        public int DiceRoll()
+        {
+            Die1 = _rand.Next(1, 7);
+            Die2 = _rand.Next(1, 7);
+
+            return Die1 + Die2;
+        }
+    }
+}
