d149a773ff6992a58573feb5b5bd09c045124eb8
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-10 17:54:50 +0100

    adds statistics class with win lose counting arrays. Adds ProbabilityOfWinning method and method to display the statistics

---
+++ _15_GameOfCraps/Properties/Statistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _15_GameOfCraps.Properties
+{
+    public class Statistics
+    {
+        public static int[] Wins = new int[22];
+        public static int[] Loses = new int[22];
+
+        public void DisplayStatistics()
+        {
+            double probability;
+            probability = ProbabilityOfWinning();
+            Console.WriteLine($"Chances of winning the game of Craps are {probability:P}");
+        }
+
+        public double ProbabilityOfWinning()
+        {
+            return (Wins.Sum() * 1.0) / (Wins.Sum() + Loses.Sum());
+        }
+    }
+}
